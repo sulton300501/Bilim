cd09c1e7fe80561a5bce85cb55da637c49ceae9b
Sulton <your-email@example.com>
2025-04-03 20:58:58 +0500

    Init hh

--- app/database.py
+++ app/database.py
@@ -1,8 +1,16 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import sessionmaker, Session , declarative_base
 from typing import Annotated
 from fastapi import Depends
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
 
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./DeliveryD.db"
--- app/models.py
+++ app/models.py
@@ -1,10 +1,21 @@
-from sqlalchemy import Integer, String, Boolean, Column
+from sqlalchemy.orm import mapped_column , Mapped , relationship
+from sqlalchemy import Integer , String , DateTime , Date
+
 from app.database import Base
+
+from datetime import datetime , date , timezone
 
 
 class User(Base):
     __tablename__="users"
-    id = Column(Integer, primary_key=True)
-    username = Column (String, nullable=False)
-    password = Column (String, nullable=False)
-    role= Column (String(10), nullable=False)
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(unique=True)
+    hashed_password: Mapped[str] = mapped_column(String(128))
+    username:Mapped[str] = mapped_column(String(32) , unique=True)
+    first_name: Mapped[str] = mapped_column(String(32))
+    last_name:Mapped[str] = mapped_column(String(32))
+    birthdate:Mapped[date] = mapped_column(Date)
+    joined_at:Mapped[datetime] = mapped_column(DateTime , default=datetime.now(timezone.utc))
+    is_active:Mapped[bool] = mapped_column(default=True)
+    is_staff:Mapped[bool] = mapped_column(default=False)
+    is_superuser:Mapped[bool] = mapped_column(default=False)
